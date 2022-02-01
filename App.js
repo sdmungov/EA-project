f2588f876f04f225b435d4e4648649988df1fb38
sdmungov <stevenmun@yahoo.com>
2022-02-01 00:38:32 -0500

    functionality is complete, still need to work on polished design

--- App.js
+++ App.js
@@ -32,28 +32,58 @@ const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [options, setOptions] = useState([]);
   const [currentPlayer, setCurrentPlayer] = useState();
-  const [center, setCenter] = useState(null);
-  const [powerForward, setPowerForward] = useState(null);
-  const [smallForward, setSmallForward] = useState(null);
-  const [pointGuard, setPointGuard] = useState(null);
-  const [shootingGuard, setShootingGuard] = useState(null);
+  const [center, setCenter] = useState([]);
+  const [powerForward, setPowerForward] = useState([]);
+  const [smallForward, setSmallForward] = useState([]);
+  const [pointGuard, setPointGuard] = useState([]);
+  const [shootingGuard, setShootingGuard] = useState([]);
+  const [centerFilter, setCenterFilter] = useState(true);
+  const [forwardFilter, setForwardFilter] = useState(true);
+  const [guardFilter, setGuardFilter] = useState(true);
+
+  const filteredPlayers = playersArr.filter((player) => {
+    const isC = center.filter((c) => c.personId === player.personId).length > 0;
+    const isPF =
+      powerForward.filter((pf) => pf.personId === player.personId).length > 0;
+    const isSF =
+      smallForward.filter((sf) => sf.personId === player.personId).length > 0;
+    const isPG =
+      pointGuard.filter((pg) => pg.personId === player.personId).length > 0;
+    const isSG =
+      shootingGuard.filter((sg) => sg.personId === player.personId).length > 0;
+    const notOnTeam = !isC && !isPF && !isSF && !isPG && !isSG;
+    let isCenter;
+    let isForward;
+    let isGuard;
+    if (centerFilter) {
+      isCenter = player.pos.includes("C");
+    }
+    if (forwardFilter) {
+      isForward = player.pos.includes("F");
+    }
+    if (guardFilter) {
+      isGuard = player.pos.includes("G");
+    }
+    const positionFiltered = isCenter || isForward || isGuard;
+    return notOnTeam && positionFiltered;
+  });
 
   const updateTeam = (key) => {
     switch (key) {
       case "Center":
-        setCenter(currentPlayer);
+        setCenter([...center, currentPlayer]);
         break;
       case "Power Forward":
-        setPowerForward(currentPlayer);
+        setPowerForward([...powerForward, currentPlayer]);
         break;
       case "Small Forward":
-        setSmallForward(currentPlayer);
+        setSmallForward([...smallForward, currentPlayer]);
         break;
       case "Point Guard":
-        setPointGuard(currentPlayer);
+        setPointGuard([...pointGuard, currentPlayer]);
         break;
       case "Shooting Guard":
-        setShootingGuard(currentPlayer);
+        setShootingGuard([...shootingGuard, currentPlayer]);
         break;
       default:
         break;
@@ -86,6 +116,25 @@ const App = () => {
               </TouchableOpacity>
             );
           })}
+          <TouchableOpacity
+            style={[
+              styles.posOptionCard,
+              { borderColor: "red", borderWidth: 0, backgroundColor: "red" },
+            ]}
+            key={"cancel"}
+            onPress={() => {
+              setModalVisible(false);
+            }}
+          >
+            <Text
+              style={[
+                styles.playerCardText,
+                { fontSize: 18, fontWeight: "bold", color: "white" },
+              ]}
+            >
+              Cancel
+            </Text>
+          </TouchableOpacity>
         </View>
       </Modal>
     );
@@ -135,8 +184,8 @@ const App = () => {
               },
             ]}
           >
-            No {positionTitle} selected, choose a {positionTitle} from available
-            players below
+            No {positionTitle}s selected, choose a {positionTitle} from
+            available players below
           </Text>
         </View>
       </View>
@@ -172,22 +221,37 @@ const App = () => {
       setCurrentPlayer(player);
     };
 
-    const handleRemove = (key) => {
+    const handleRemove = (personId, key) => {
       switch (key) {
         case "Center":
-          setCenter(null);
+          const filteredCenter = center.filter((player) => {
+            return player.personId !== personId;
+          });
+          setCenter(filteredCenter);
           break;
         case "Power Forward":
-          setPowerForward(null);
+          const filteredPowerForward = powerForward.filter((player) => {
+            return player.personId !== personId;
+          });
+          setPowerForward(filteredPowerForward);
           break;
         case "Small Forward":
-          setSmallForward(null);
+          const filteredSmallForward = smallForward.filter((player) => {
+            return player.personId !== personId;
+          });
+          setSmallForward(filteredSmallForward);
           break;
         case "Point Guard":
-          setPointGuard(null);
+          const filteredPointGuard = pointGuard.filter((player) => {
+            return player.personId !== personId;
+          });
+          setPointGuard(filteredPointGuard);
           break;
         case "Shooting Guard":
-          setShootingGuard(null);
+          const filteredShootingGuard = shootingGuard.filter((player) => {
+            return player.personId !== personId;
+          });
+          setShootingGuard(filteredShootingGuard);
           break;
         default:
           break;
@@ -217,7 +281,9 @@ const App = () => {
         <TouchableOpacity
           style={styles.addButton}
           onPress={() =>
-            action === "Add" ? handleAdd(item.pos, item) : handleRemove(posKey)
+            action === "Add"
+              ? handleAdd(item.pos, item)
+              : handleRemove(item.personId, posKey)
           }
         >
           <Text style={styles.addButtonText}>{action}</Text>
@@ -236,7 +302,7 @@ const App = () => {
         style={styles.scrollView}
         contentContainerStyle={styles.scrollContent}
       >
-        <Text style={styles.header}>New Team</Text>
+        <Text style={styles.header}>Team Builder</Text>
         <View style={styles.inputRows}>
           <Text style={styles.teamDetailsInput}>Team Name:</Text>
           <TextInput
@@ -257,55 +323,124 @@ const App = () => {
           Add players to your roster to fill the required positions.
         </Text>
         <View style={styles.divider} />
-        <Text style={styles.sectionTitle}>My Team:</Text>
-        <Text style={styles.positionTitle}>- Center -</Text>
-        {center ? (
-          <PlayerCard item={center} action="Remove" posKey="Center" />
+        <Text style={styles.sectionTitle}>- My Team -</Text>
+        <Text style={styles.positionTitle}>Centers:</Text>
+        {center.length ? (
+          center.map((player) => {
+            return (
+              <PlayerCard
+                item={player}
+                action="Remove"
+                posKey="Center"
+                key={player.personId}
+              />
+            );
+          })
         ) : (
           <EmptyCard positionTitle="Center" />
         )}
-        <Text style={styles.positionTitle}>- Power Forward -</Text>
-        {powerForward ? (
-          <PlayerCard
-            item={powerForward}
-            action="Remove"
-            posKey="Power Forward"
-          />
+        <Text style={styles.positionTitle}>Power Forwards:</Text>
+        {powerForward.length ? (
+          powerForward.map((player) => {
+            return (
+              <PlayerCard
+                item={player}
+                action="Remove"
+                posKey="Power Forward"
+                key={player.personId}
+              />
+            );
+          })
         ) : (
           <EmptyCard positionTitle="Power Forward" />
         )}
-        <Text style={styles.positionTitle}>- Small Forward -</Text>
-        {smallForward ? (
-          <PlayerCard
-            item={smallForward}
-            action="Remove"
-            posKey="Small Forward"
-          />
+        <Text style={styles.positionTitle}>Small Forwards:</Text>
+        {smallForward.length ? (
+          smallForward.map((player) => {
+            return (
+              <PlayerCard
+                item={player}
+                action="Remove"
+                posKey="Small Forward"
+                key={player.personId}
+              />
+            );
+          })
         ) : (
           <EmptyCard positionTitle="Small Forward" />
         )}
-        <Text style={styles.positionTitle}>- Point Guard -</Text>
-        {pointGuard ? (
-          <PlayerCard item={pointGuard} action="Remove" posKey="Point Guard" />
+        <Text style={styles.positionTitle}>Point Guards:</Text>
+        {pointGuard.length ? (
+          pointGuard.map((player) => {
+            return (
+              <PlayerCard
+                item={player}
+                action="Remove"
+                posKey="Point Guard"
+                key={player.personId}
+              />
+            );
+          })
         ) : (
           <EmptyCard positionTitle="Point Guard" />
         )}
-        <Text style={styles.positionTitle}>- Shooting Guard -</Text>
-        {shootingGuard ? (
-          <PlayerCard
-            item={shootingGuard}
-            action="Remove"
-            posKey="Shooting Guard"
-          />
+        <Text style={styles.positionTitle}>Shooting Guards:</Text>
+        {shootingGuard.length ? (
+          shootingGuard.map((player) => {
+            return (
+              <PlayerCard
+                item={player}
+                action="Remove"
+                posKey="Shooting Guard"
+                key={player.personId}
+              />
+            );
+          })
         ) : (
           <EmptyCard positionTitle="Shooting Guard" />
         )}
         <View style={styles.divider} />
-        <Text style={styles.sectionTitle}>Players Available:</Text>
+        <Text style={styles.sectionTitle}>- Players Available -</Text>
+        <View style={styles.filterContainer}>
+          <Text>Filter by Position:</Text>
+          <View style={styles.filterGroup}>
+            <TouchableOpacity
+              hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+              style={[
+                styles.checkbox,
+                { backgroundColor: centerFilter ? "blue" : "transparent" },
+              ]}
+              onPress={() => setCenterFilter(!centerFilter)}
+            />
+            <Text style={styles.filterText}>Center</Text>
+          </View>
+          <View style={styles.filterGroup}>
+            <TouchableOpacity
+              hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+              style={[
+                styles.checkbox,
+                { backgroundColor: forwardFilter ? "blue" : "transparent" },
+              ]}
+              onPress={() => setForwardFilter(!forwardFilter)}
+            />
+            <Text style={styles.filterText}>Forward</Text>
+          </View>
+          <View style={styles.filterGroup}>
+            <TouchableOpacity
+              hitSlop={{ top: 16, bottom: 16, left: 16, right: 16 }}
+              style={[
+                styles.checkbox,
+                { backgroundColor: guardFilter ? "blue" : "transparent" },
+              ]}
+              onPress={() => setGuardFilter(!guardFilter)}
+            />
+            <Text style={styles.filterText}>Guard</Text>
+          </View>
+        </View>
         {!loading && (
           <FlatList
             style={styles.playerList}
-            data={playersArr}
+            data={filteredPlayers}
             renderItem={({ item }) => <PlayerCard item={item} action="Add" />}
             keyExtractor={(item) => `${item.personId}`}
           />
@@ -322,7 +457,6 @@ const styles = StyleSheet.create({
   },
   innerWrapper: {
     backgroundColor: "transparent",
-    flex: 1,
   },
   scrollContent: {
     paddingHorizontal: 16,
@@ -355,14 +489,34 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
   positionTitle: {
-    alignSelf: "center",
     fontWeight: "bold",
     fontSize: 20,
   },
   sectionTitle: {
+    alignSelf: "center",
     fontWeight: "bold",
     fontSize: 22,
+    marginBottom: 24,
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 16,
+  },
+  filterGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  filterText: {
+    marginLeft: 4,
+  },
+  checkbox: {
+    height: 16,
+    width: 16,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: "blue",
   },
   playerList: {
     height: height,
